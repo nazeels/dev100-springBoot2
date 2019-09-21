80dc1e09a30e709ccbf30546b1ea0183e044e36c
nazeels <nazeel.ak@gmail.com>
2019-09-21 10:40:58 +0530

    Initial commit, first step of learning

---
+++ src/main/java/com/example/amal/AmalApplication.java
@@ -0,0 +1,13 @@
+package com.example.amal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmalApplication.class, args);
+	}
+
+}
